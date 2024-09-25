9e6c47d28ae58cc852568cc0188f32ebe78f4e59
tima2208-stack <149979903+tima2208-stack@users.noreply.github.com>
2024-09-25 18:52:47 +0300

    Create task2.sh

---
+++ pr2/task2.sh
@@ -0,0 +1,12 @@
+{
+  "name": "my-express-app",
+  "version": "1.0.0",
+  "description": "A simple Express application",
+  "main": "app.js",
+  "scripts": {
+    "start": "node app.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1"
+  }
+}
