24310c2b5cb39c22efc3578e8069f40f9d631028
tima2208-stack <149979903+tima2208-stack@users.noreply.github.com>
2024-11-13 18:38:01 +0300

    Create task3.sh

---
+++ pr4/task3.sh
@@ -0,0 +1,29 @@
+*   a457d748f0dab75b4c642e964172887de3ef4e3e Merge: 48ce283 d731ba8
+|\  Author: Coder 2 <coder2@corp.com>
+| | Date:   Sun Oct 11 11:27:09 2020 +0300
+| | 
+| |     fix: решен конфликт в разделе об авторах
+| | 
+| * d731ba84014d603384cc3287a8ea9062dbb92303
+| | Author: Coder 1 <coder1@corp.com>
+| | Date:   Sun Oct 11 11:22:52 2020 +0300
+| | 
+| |     docs: добавлена информация об авторе coder1
+| | 
+* | 48ce28336e6b3b983cbd6323500af8ec598626f1
+|/  Author: Coder 2 <coder2@corp.com>
+|   Date:   Sun Oct 11 11:24:00 2020 +0300
+|   
+|       docs: добавлена информация об авторе coder2
+| 
+* ba9dfe9cb24316694808a347e8c36f8383d81bbe
+| Author: Coder 2 <coder2@corp.com>
+| Date:   Sun Oct 11 11:21:26 2020 +0300
+| 
+|     docs: добавлен файл readme.md с описанием программы
+| 
+* 227d84c89e60e09eebbce6c0b94b41004a4541a4
+  Author: Coder 1 <coder1@corp.com>
+  Date:   Sun Oct 11 11:11:46 2020 +0300
+  
+      first commit
